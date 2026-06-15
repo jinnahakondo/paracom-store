447e880ff8698c332fdf7d21174acc0368108c79
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-06-15 16:38:35 +0600

    added api to update & delete user by id" git pus

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -6,6 +6,8 @@ interface routeProps {
     params: Promise<{ id: string }>
 }
 
+
+//get a user by id
 export async function GET(req: NextRequest, { params }: routeProps) {
     try {
         const { id } = await params
@@ -31,3 +33,64 @@ export async function GET(req: NextRequest, { params }: routeProps) {
         })
     }
 }
+
+//update a user by id
+export async function PATCH(req: NextRequest, { params }: routeProps) {
+    try {
+        const { id } = await params
+
+        const payload = await req.json()
+
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            payload,
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedUser) {
+            return response.error({
+                message: "user not found",
+                status: 404,
+            });
+        }
+
+        return response.success({
+            message: "user updated successfully",
+            data: updatedUser
+        })
+
+    } catch (error: any) {
+        return response.error({
+            message: 'failed to update user',
+            error: error.message
+        })
+    }
+}
+
+
+//delete a user by id
+export async function DELETE(req: NextRequest, { params }: routeProps) {
+    try {
+        const { id } = await params;
+
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return response.error({
+                message: "user not found",
+                status: 404,
+            });
+        }
+
+        return response.success({
+            message: "user deleted successfull"
+        })
+
+    } catch (error: any) {
+        return response.error({
+            message: "failed to delete user",
+            error: error.message
+        })
+    }
+}
+
